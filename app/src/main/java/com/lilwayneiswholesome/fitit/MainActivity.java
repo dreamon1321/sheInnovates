59414f90d5d5a1a2ff29532140553fda36962e26
Joshua Maciak <jtm97@pitt.edu>
2019-01-26 19:37:18 -0500

    rebranded

--- app/src/main/java/com/lilwayneiswholesome/fitit/MainActivity.java
+++ app/src/main/java/com/lilwayneiswholesome/fitit/MainActivity.java
@@ -1,12 +1,15 @@
 package com.lilwayneiswholesome.fitit;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.api.services.vision.v1.Vision;
 import com.lilwayneiswholesome.fitit.util.VisionHelper;
@@ -24,44 +27,60 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
     private static final String CLOUD_VISION_API_KEY = "";
     private static final String TAG = "MainActivity";
+    private static final int REQUEST_IMAGE_CAPTURE = 123;
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resultTextView = findViewById(R.id.resultTextView);
         Button button = findViewById(R.id.button);
         button.setOnClickListener(this::onSelectPhotoButtonClicked);
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            final Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            Observable.fromCallable(() -> {
+                String result = null;
+/*                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 8;
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                bitmap.recycle();*/
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+                result = VisionHelper.doVision(byteArray);
+                return result;
+            })
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe((result) -> {
+                        resultTextView.setText(result);
+                    }, error -> {
+                        Log.e(TAG, "Error: " + error);
+                    });
+
+        }
+    }
+
     public void onSelectPhotoButtonClicked(View view) {
-        int rid = getResources()
-                .getIdentifier("test_chair","raw", getPackageName());
-
-
-        Observable.fromCallable(() -> {
-            String result = null;
-            InputStream is = getResources().openRawResource(rid);
-            byte[] bytes = IOUtils.toByteArray(is);
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 8;
-            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-            byte[] byteArray = stream.toByteArray();
-            bitmap.recycle();
-
-            result = VisionHelper.doVision(byteArray);
-            return result;
-        })
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-                .subscribe((result) -> {
-                    Log.e(TAG, "result: " + result);
-                }, error -> {
-            Log.e(TAG, "Error: " + error);
-        });
-
+        dispatchTakePictureIntent();
     }
 
 
